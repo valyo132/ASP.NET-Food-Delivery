cf44519cef7aa4e4fe62acdccfb7d2d4f53dd7d4
Valentin Markov <101991279+valyo132@users.noreply.github.com>
2023-06-03 17:24:08 +0300

    Initial commit
    Roles for User and Administrator
    Added ApplicationUser

---
+++ GustoExpress/GustoExpress.Data.Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GustoExpress.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
---
+++ GustoExpress/GustoExpress.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GustoExpress.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GustoExpress.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+    }
+}
