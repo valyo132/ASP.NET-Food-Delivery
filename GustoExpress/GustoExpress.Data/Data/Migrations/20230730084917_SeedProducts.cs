ba05d0d17b1660b0d1aec9bd9f8d5ba6e1b70935
Valentin Markov <101991279+valyo132@users.noreply.github.com>
2023-07-30 14:04:13 +0300

    seeding products fixed

--- GustoExpress/GustoExpress.Data/Data/Migrations/20230730084917_SeedProducts.cs
+++ GustoExpress/GustoExpress.Data/Data/Migrations/20230730084917_SeedProducts.cs
@@ -16,19 +16,15 @@ namespace GustoExpress.Web.Data.Migrations
                 columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
                 values: new object[] { "cbba6db5-7180-405b-a3ba-9f1c10f500a1", "AQAAAAEAACcQAAAAEJBzMkmkvusKIhM13nevotYSRGkvGMC2aTplC/TPiQ2YqDy1Z0xCMdCMnHYVUsMOvw==", "00accc74-758b-4bfe-b53e-90a28992f2c4" });
 
-            migrationBuilder.UpdateData(
+            migrationBuilder.InsertData(
                 table: "Products",
-                keyColumn: "Id",
-                keyValue: new Guid("34e46ce4-b7d1-4642-9b8a-15d1666f0dbe"),
-                column: "ImageURL",
-                value: "/images/Products/2af60ede-d017-4c1a-9dda-27bc4fd9fb6b.jpg");
+                columns: new[] { "Id", "Category", "Description", "Discount", "Grams", "ImageURL", "IsDeleted", "Name", "Price", "RestaurantId" },
+                values: new object[] { new Guid("34e46ce4-b7d1-4642-9b8a-15d1666f0dbe"), 7, "Simple cheeseburger", null, 150m, "/images/Products/2af60ede-d017-4c1a-9dda-27bc4fd9fb6b.jpg", false, "Cheeseburger", 4.00m, new Guid("c5f0aaf2-99f2-43de-8e48-9a8fb97ccc44") });
 
-            migrationBuilder.UpdateData(
+            migrationBuilder.InsertData(
                 table: "Products",
-                keyColumn: "Id",
-                keyValue: new Guid("b55643d9-5372-442b-b17d-c463bb1f4eaf"),
-                column: "ImageURL",
-                value: "/images/Products/0d3fa9fe-d37c-4769-9f8b-bac4424ee3b1.png");
+                columns: new[] { "Id", "Category", "Description", "Discount", "Grams", "ImageURL", "IsDeleted", "Name", "Price", "RestaurantId" },
+                values: new object[] { new Guid("b55643d9-5372-442b-b17d-c463bb1f4eaf"), 6, "Very taste bubble tea", null, 300m, "/images/Products/0d3fa9fe-d37c-4769-9f8b-bac4424ee3b1.png", false, "Bubble tea", 3.00m, new Guid("c5f0aaf2-99f2-43de-8e48-9a8fb97ccc44") });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
